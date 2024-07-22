922909a2c85cf88e99e0db327e961d3f9bcb6ed6
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-07-22 21:42:04 +0300

    Added syscalls for cd, bg commands, and ^z signal and handled what happens after an error happen.

--- simple_shell.c
+++ simple_shell.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <dirent.h>
+#include <signal.h>
 
 extern char **environ;
 
@@ -261,7 +262,7 @@ void run(char **line)
 
         if (execve(line[0], line, NULL) == -1)
         {
-            perror("PERMISSION OK AND ERROR : ");
+            perror("PERMISSION OK AND ERROR");
             exit(0);
         }
     }
@@ -277,8 +278,8 @@ void run(char **line)
         
         if (execve(line[0], line, NULL) == -1)
         {
-            perror("PERMISSION DENIED AND ERROR : ");
-            exit(0);
+            perror("PERMISSION DENIED AND ERROR");
+	    exit(0);
         }
     }
 }
@@ -290,70 +291,104 @@ void run(char **line)
  */
 int main(void)
 {
-    char *buffer = malloc(1024);
-    size_t len = 1024;
+	char *buffer = malloc(1024);
+	size_t len = 1024;
 
-    while (1)
-    {
-        printf("zkrallah$ ");
-        if (getline(&buffer, &len, stdin) == -1)
-        {
-            exit(98);
-        }
+	while (1)
+	{
+		printf("zkrallah$ ");
+		if (getline(&buffer, &len, stdin) == -1)
+		{
+			exit(98);
+		}
 
-        if (_strlen(buffer) == 1 && buffer[0] == '\n')
-        {
-            continue;
-        }
-        else if (_strcmp(buffer, "exit\n") == 0)
-        {
-            exit(0);
-        }
-        else if (_strcmp(buffer, "env\n") == 0)
-        {
-            unsigned int j;
-            j = 0;
+		if (_strlen(buffer) == 1 && buffer[0] == '\n')
+		{
+			continue;
+		}
+		else if (_strcmp(buffer, "exit\n") == 0)
+		{
+			exit(0);
+		}
+		else if (_strcmp(buffer, "env\n") == 0)
+		{
+			unsigned int j;
+			j = 0;
 
-            while(environ[j])
-            {
-                printf("%s\n", environ[j++]);
-            }
+			while(environ[j])
+			{
+				printf("%s\n", environ[j++]);
+			}
 
-            continue;
-        }
+			continue;
+		}
 
-        int nwords = 0;
-        char **arr = str_split (buffer, &nwords);
-        char *argv[nwords + 1];
-        for (int i = 0; i < nwords; i++){
-            if (arr[i][_strlen(arr[i]) - 1] == '\n')
-            {
-                arr[i][_strlen(arr[i]) - 1] = '\0';
-            }
-            argv[i] = arr[i];
-        }
-        argv[nwords] = NULL;
+		int nwords = 0;
+		char **arr = str_split (buffer, &nwords);
+		char *argv[nwords + 1];
+		for (int i = 0; i < nwords; i++)
+		{
+			if (arr[i][_strlen(arr[i]) - 1] == '\n')
+			{
+				arr[i][_strlen(arr[i]) - 1] = '\0';
+			}
 
-        pid_t child_pid = 1;
-        int status;
+			argv[i] = arr[i];
+		}
 
-        if (child_pid != 0)
-        {
-            child_pid = fork();
-            if (child_pid == -1)
-            {
-                printf("error");
-                return (1);
-            }
-            wait(&status);
-        }
+		argv[nwords] = NULL;
 
-        if (child_pid == 0)
-        {
-            run(argv);
-        }
+		pid_t child_pid = 1;
+		int status;
 
-    }
+		if (child_pid != 0)
+		{
+			child_pid = fork();
+			if (child_pid == -1)
+			{
+				printf("error");
+				return (1);
+			}
+
+			wait(&status);
+		}
+
+		if (child_pid == 0)
+		{
+			if (_strcmp(argv[0], "cd") == 0)
+			{
+				if (chdir(argv[1]) != 0)
+				{
+					perror("chdir failed");
+				}
+
+				exit(0);
+			}
+			else if (_strcmp(argv[0], "stop") == 0)
+			{
+				if (kill(atoi(argv[1]), SIGSTOP) == -1)
+				{
+					perror("Error sending SIGSTOP");
+				}
+
+				exit(0);
+			}
+			else if (_strcmp(argv[0], "bg") == 0)
+			{
+				if (kill(atoi(argv[1]), SIGCONT) == -1)
+				{
+					perror("Error sendinf SIGCONT");
+				}
+
+				exit(0);
+			}
+			else
+			{
+				run(argv);
+			}
+		}
+
+ 	}
 
     return (0);
 }
